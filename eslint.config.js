910a6cfa667abee1a1f5184d1ca6265c7abcd1db
NNobutoshi <cooke222000@ybb.ne.jp>
2025-04-29 12:49:25 +0900

    key-spacing のルールを一部変更。 .vscode/settings.json でflat config を有効にする。

--- eslint.config.js
+++ eslint.config.js
@@ -1,64 +1,73 @@
 import path              from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import globals from 'globals';
-
 import { defineConfig } from 'eslint/config';
 import { FlatCompat }   from '@eslint/eslintrc';
 import js               from '@eslint/js';
 
-const __filename = fileURLToPath( import.meta.url );
-const __dirname = path.dirname( __filename );
-const compat = new FlatCompat( {
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-} );
+import globals from 'globals';
 
-export default defineConfig( [ {
-  extends: compat.extends( 'jquery' ),
+const
+  __filename = fileURLToPath( import.meta.url )
+  ,__dirname = path.dirname( __filename )
+  ,compat    = new FlatCompat( {
+    baseDirectory     : __dirname,
+    recommendedConfig : js.configs.recommended,
+    allConfig         : js.configs.all
+  } )
+;
+export default defineConfig(
+  [ {
+    extends : compat.extends( 'jquery' ),
 
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      $: true,
+    languageOptions : {
+      globals : {
+        ...globals.browser,
+        ...globals.node,
+        $ : true,
+      },
+
+      ecmaVersion : 2022,
+      sourceType : 'module',
     },
 
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
+    rules : {
+      indent            : [ 'error', 2 ],
+      'linebreak-style' : [ 'error', 'unix' ],
+      quotes            : [ 'error', 'single' ],
+      semi              : [ 'error', 'always' ],
+      'comma-spacing'   : [ 'off' ],
+      'comma-style'     : [ 'off' ],
+      'key-spacing' : [
+        'error',
+        {
+          'mode' : 'minimum',
+          'beforeColon' : true,
+          'afterColon'  : true
+        }
+      ],
+      'comma-dangle'    : [ 'off' ],
+      'no-unused-vars'  : [ 'error', {
+        args              : 'all',
+        argsIgnorePattern : '^_|^e|^index',
+      } ],
 
-  rules: {
-    indent: [ 'error', 2 ],
-    'linebreak-style': [ 'error', 'unix' ],
-    quotes: [ 'error', 'single' ],
-    semi: [ 'error', 'always' ],
-    'comma-spacing': [ 'off' ],
-    'comma-style': [ 'off' ],
-    'key-spacing': [ 'off' ],
-    'comma-dangle': [ 'off' ],
+      camelcase : [ 'off' ],
 
-    'no-unused-vars': [ 'error', {
-      args: 'all',
-      argsIgnorePattern: '^_|^e|^index',
-    } ],
+      'lines-around-comment' : [ 'error', {
+        beforeLineComment : false,
+      } ],
 
-    camelcase: [ 'off' ],
-
-    'lines-around-comment': [ 'error', {
-      beforeLineComment: false,
-    } ],
-
-    'operator-linebreak': [ 'error', 'after', {
-      overrides: {
-        '?': 'ignore',
-        ':': 'ignore',
-        '||': 'ignore',
-        '&&': 'ignore',
-        '+': 'ignore',
-        '-': 'ignore',
-      },
-    } ],
-  },
-} ] );
+      'operator-linebreak' : [ 'error', 'after', {
+        overrides : {
+          '?'  : 'ignore',
+          ':'  : 'ignore',
+          '||' : 'ignore',
+          '&&' : 'ignore',
+          '+'  : 'ignore',
+          '-'  : 'ignore',
+        },
+      } ],
+    },
+  } ]
+);
