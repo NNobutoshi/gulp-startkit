d2270dbf00969112d541f7e0b2bb872ed50eb4ba
NNobutoshi <cooke222000@ybb.ne.jp>
2025-05-04 16:47:23 +0900

    svg_lint.js option をconfig.js で設定可能にする。

--- gulpkit/lib/svg_lint.js
+++ gulpkit/lib/svg_lint.js
@@ -1,26 +1,37 @@
 import SVGLint from 'svglint';
 import through from 'through2';
 
+const
+  defaultSettings = {
+    debug : true,
+    config : {},
+  }
+;
+
 /**
  * svg をsrc にするタスク用。
  * エラーが拾いにくいため。
  * @returns {Object} - Gulp stream
  */
-export default function svg_lint() {
-
-  return through.obj( _transform );
-
-  async function _transform( file, enc, callback ) {
-    const
-      contents = String( file.contents )
-      ,linting = await SVGLint.lintSource( contents, { debug : true, config : {} } )
-    ;
-    linting.on( 'done', () => {
-      if ( linting.state === 'error' || linting.valid === false ) {
-        return callback( new Error( `Linting failed (${ linting.state })\n${ file.path }` ) );
+export default function svg_lint( options ) {
+  const settings = { ...defaultSettings, ...options };
+  return through.obj(
+    async function _transform( file, enc, callback ) {
+      try {
+        const
+          contents = String( file.contents )
+          ,linting = await SVGLint.lintSource( contents, settings )
+        ;
+        linting.on( 'done', () => {
+          if ( linting.state === 'error' || linting.valid === false ) {
+            return callback( new Error( `Linting failed (${ linting.state })\n${ file.path }` ) );
+          }
+          callback( null, file );
+        } );
+        linting.lint();
+      } catch ( err ) {
+        callback( err );
       }
-      callback( null, file );
-    } );
-    linting.lint();
-  }
+    }
+  );
 }
